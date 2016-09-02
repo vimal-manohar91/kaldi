289d2b9c7c9dbf2d17ce3eabb760a00da76ce08f
Vimal Manohar <vimal.manohar91@gmail.com>
2016-09-02 14:46:06 -0400

    Minor fix to AMI scripts

--- egs/ami/s5b/local/nnet3/run_ivector_common.sh
+++ egs/ami/s5b/local/nnet3/run_ivector_common.sh
@@ -184,7 +184,7 @@ if [ $stage -le 7 ]; then
   # Also extract iVectors for the test data, but in this case we don't need the speed
   # perturbation (sp) or small-segment concatenation (comb).
   for data in dev eval; do
-    steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj "$nj" \
+    steps/online/nnet2/extract_ivectors_online.sh --cmd "$train_cmd" --nj 10 \
       data/${mic}/${data}_hires exp/$mic/nnet3${nnet3_affix}/extractor \
       exp/$mic/nnet3${nnet3_affix}/ivectors_${data}_hires
   done
