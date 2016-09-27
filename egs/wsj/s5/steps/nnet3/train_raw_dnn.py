a193b380701fab8dba1ddd501f8cff6f6317e546
Vimal Manohar <vimal.manohar91@gmail.com>
2016-09-27 00:51:50 -0400

    raw_python_script: Adding raw nnet training

--- egs/wsj/s5/steps/nnet3/train_raw_dnn.py
+++ egs/wsj/s5/steps/nnet3/train_raw_dnn.py
@@ -27,7 +27,7 @@ handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s - %(levelname)s ] %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.info('Starting DNN trainer (train_dnn.py)')
+logger.info('Starting DNN trainer (train_raw_dnn.py)')
 
 
 def GetArgs():
--- egs/wsj/s5/steps/nnet3/train_raw_rnn.py
+++ egs/wsj/s5/steps/nnet3/train_raw_rnn.py
@@ -27,7 +27,7 @@ handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)s - %(funcName)s - %(levelname)s ] %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.info('Starting RNN trainer (train_rnn.py)')
+logger.info('Starting RNN trainer (train_raw_rnn.py)')
 
 
 def GetArgs():
