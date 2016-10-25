944536c4ce9c79a451ab71994bc5779294e6b1fd
Vimal Manohar <vimal.manohar91@gmail.com>
2016-10-25 10:38:23 -0400

    raw_python_script: Removing rnn_train_lib.py

--- egs/wsj/s5/steps/nnet3/libs/train_lib.py
+++ egs/wsj/s5/steps/nnet3/libs/train_lib.py
@@ -23,20 +23,21 @@ formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)s - %(funcName
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# this is the main method which differs between RNN and DNN training
 def TrainNewModels(dir, iter, srand, num_jobs,
                    num_archives_processed, num_archives,
-                   raw_model_string, egs_dir, frames_per_eg,
+                   raw_model_string, egs_dir,
                    left_context, right_context,
                    momentum, max_param_change,
-                   shuffle_buffer_size, minibatch_size,
-                   cache_read_opt, run_opts,
+                   shuffle_buffer_size, minibatch_size, frames_per_eg,
+                   cache_read_opt, run_opts, min_deriv_time = None,
                    extra_egs_copy_cmd = ""):
     # We cannot easily use a single parallel SGE job to do the main training,
     # because the computation of which archive and which --frame option
     # to use for each job is a little complex, so we spawn each one separately.
     # this is no longer true for RNNs as we use do not use the --frame option
     # but we use the same script for consistency with FF-DNN code
+
+    chunk_level_training = False if frames_per_eg > 0 else True
 
     context_opts="--left-context={0} --right-context={1}".format(
                   left_context, right_context)
@@ -45,7 +46,9 @@ def TrainNewModels(dir, iter, srand, num_jobs,
         k = num_archives_processed + job - 1 # k is a zero-based index that we will derive
                                                # the other indexes from.
         archive_index = (k % num_archives) + 1 # work out the 1-based archive index.
-        frame = (k / num_archives) % frames_per_eg
+
+        if not chunk_level_training:
+            frame = (k / num_archives) % frames_per_eg
 
         cache_write_opt = ""
         if job == 1:
@@ -58,16 +61,21 @@ def TrainNewModels(dir, iter, srand, num_jobs,
   nnet3-train {parallel_train_opts} {cache_read_opt} {cache_write_opt} \
   --print-interval=10 --momentum={momentum} \
   --max-param-change={max_param_change} \
-  "{raw_model}" \
-  "ark,bg:nnet3-copy-egs --frame={frame} {context_opts} ark:{egs_dir}/egs.{archive_index}.ark ark:- | nnet3-shuffle-egs --buffer-size={shuffle_buffer_size} --srand={srand} ark:- ark:-| nnet3-merge-egs --minibatch-size={minibatch_size} --measure-output-frames=false --discard-partial-minibatches=true ark:- ark:- |{extra_egs_copy_cmd}" \
+  {optimization_opts} "{raw_model}" \
+  "ark,bg:nnet3-copy-egs {frame_opts} {context_opts} ark:{egs_dir}/egs.{archive_index}.ark ark:- | nnet3-shuffle-egs --buffer-size={shuffle_buffer_size} --srand={srand} ark:- ark:-| nnet3-merge-egs --minibatch-size={minibatch_size} --measure-output-frames=false --discard-partial-minibatches=true ark:- ark:- |{extra_egs_copy_cmd}" \
   {dir}/{next_iter}.{job}.raw
           """.format(command = run_opts.command,
                      train_queue_opt = run_opts.train_queue_opt,
                      dir = dir, iter = iter, srand = iter + srand, next_iter = iter + 1, job = job,
                      parallel_train_opts = run_opts.parallel_train_opts,
                      cache_read_opt = cache_read_opt, cache_write_opt = cache_write_opt,
-                     frame = frame,
+                     frame_opts = ""
+                                  if chunk_level_training
+                                  else "--frame={0}".format(frame),
                      momentum = momentum, max_param_change = max_param_change,
+                     optimization_opts = "--optimization.min-deriv-time={0}".format(min_deriv_time)
+                                         if min_deriv_time is not None
+                                         else "",
                      raw_model = raw_model_string, context_opts = context_opts,
                      egs_dir = egs_dir, archive_index = archive_index,
                      shuffle_buffer_size = shuffle_buffer_size,
@@ -92,10 +100,12 @@ def TrainNewModels(dir, iter, srand, num_jobs,
 def TrainOneIteration(dir, iter, srand, egs_dir,
                       num_jobs, num_archives_processed, num_archives,
                       learning_rate, minibatch_size,
-                      frames_per_eg, num_hidden_layers, add_layers_period,
+                      num_hidden_layers, add_layers_period,
                       left_context, right_context,
                       momentum, max_param_change, shuffle_buffer_size,
                       run_opts,
+                      cv_minibatch_size = 256, frames_per_eg = -1,
+                      min_deriv_time = None, shrinkage_value = 1.0,
                       get_raw_nnet_from_am = True,
                       extra_egs_copy_cmd = ""):
 
@@ -120,12 +130,14 @@ def TrainOneIteration(dir, iter, srand, egs_dir,
     # Sets off some background jobs to compute train and
     # validation set objectives
     train_lib.ComputeTrainCvProbabilities(dir, iter, egs_dir, run_opts,
+                                          mb_size = cv_minibatch_size,
                                           get_raw_nnet_from_am = get_raw_nnet_from_am,
                                           extra_egs_copy_cmd = extra_egs_copy_cmd)
 
     if iter > 0:
         # Runs in the background
         train_lib.ComputeProgress(dir, iter, egs_dir, run_opts,
+                                  mb_size = cv_minibatch_size,
                                   get_raw_nnet_from_am = get_raw_nnet_from_am,
                                   extra_egs_copy_cmd = extra_egs_copy_cmd)
 
@@ -170,12 +182,15 @@ def TrainOneIteration(dir, iter, srand, egs_dir,
     except OSError:
         pass
 
-    train_lib.TrainNewModels(dir, iter, srand, num_jobs, num_archives_processed, num_archives,
-                             raw_model_string, egs_dir, frames_per_eg,
-                             left_context, right_context,
-                             momentum, max_param_change,
-                             shuffle_buffer_size, cur_minibatch_size,
-                             cache_read_opt, run_opts, extra_egs_copy_cmd = extra_egs_copy_cmd)
+    TrainNewModels(dir, iter, srand, num_jobs,
+                   num_archives_processed, num_archives,
+                   raw_model_string, egs_dir,
+                   left_context, right_context,
+                   momentum, max_param_change,
+                   shuffle_buffer_size, cur_minibatch_size, frames_per_eg,
+                   cache_read_opt, run_opts, min_deriv_time = min_deriv_time,
+                   extra_egs_copy_cmd = extra_egs_copy_cmd)
+
     [models_to_average, best_model] = common_train_lib.GetSuccessfulModels(num_jobs, '{0}/log/train.{1}.%.log'.format(dir,iter))
     nnets_list = []
     for n in models_to_average:
@@ -187,14 +202,17 @@ def TrainOneIteration(dir, iter, srand, egs_dir,
                         dir = dir, iter = iter,
                         nnets_list = " ".join(nnets_list),
                         run_opts = run_opts,
-                        get_raw_nnet_from_am = get_raw_nnet_from_am)
+                        get_raw_nnet_from_am = get_raw_nnet_from_am,
+                        shrink = shrinkage_value)
+
     else:
         # choose the best model from different jobs
         common_train_lib.GetBestNnetModel(
                         dir = dir, iter = iter,
                         best_model_index = best_model,
                         run_opts = run_opts,
-                        get_raw_nnet_from_am = get_raw_nnet_from_am)
+                        get_raw_nnet_from_am = get_raw_nnet_from_am,
+                        shrink = shrinkage_value)
 
     try:
         for i in range(1, num_jobs + 1):
